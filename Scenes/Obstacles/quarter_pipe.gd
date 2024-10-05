ca6c899a677b22857b544372f14c2a32a4dc8233
Ciaran Woodward <ciaranwoodward@gmail.com>
2024-10-05 12:30:36 +0100

    feat: Add very basic locomotion
    I am not happy with this code, but should be starting
    point that everyone else can work with

--- Scenes/Obstacles/quarter_pipe.gd
+++ Scenes/Obstacles/quarter_pipe.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-@export var acceleration_factor = 5
+@onready var centre_node = $Centre
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +9,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func get_pipe_centre()-> Vector2:
+	return centre_node.global_position
--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,37 +1,65 @@
 extends CharacterBody2D
 
-
 const JUMP_VELOCITY = -400.0
+
+@export var MAX_SPEED = 1000
 
 @onready var aoe = $AreaOfEffect
 
-var direction : Vector2 = Vector2(1, 0);
+var gravity_effect : Vector2 = Vector2.ZERO
+var direction : Vector2 = Vector2(-1, 0);
 var acceleration = 0
 var speed = 1
+var on_floor = false
+var quarter_pipe_direction = 0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
-	if is_on_floor():
-		velocity.y = 0
+	if not on_floor:
+		gravity_effect += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and on_floor:
 		velocity.y = JUMP_VELOCITY
-
-	velocity += direction * speed
 	
 	var overlaps = aoe.get_overlapping_bodies()
 	
 	# Change behaviour based on floor
 	acceleration = 0
+	on_floor = false
 	for overlap in overlaps:
-		if "acceleration_factor" in overlap:
+		if "get_pipe_centre" in overlap:
+			if quarter_pipe_direction == 0:
+				if direction.x > 0:
+					quarter_pipe_direction = -1
+				else:
+					quarter_pipe_direction = 1
+			acceleration = 0
+			var c : Vector2 = overlap.get_pipe_centre()
+			var tangent_angle = c.angle_to_point(self.global_position) + PI/2.0
+			direction.x = cos(tangent_angle)
+			direction.y = sin(tangent_angle)
+			direction = direction.normalized()
+			direction *= quarter_pipe_direction
+			on_floor = true
+			print("PIPE")
+		elif "acceleration_factor" in overlap:
+			on_floor = true
 			acceleration = overlap.acceleration_factor
-
-	speed = speed + sign(direction.x) * acceleration
-	print(speed)
-
-	move_and_slide()
+			velocity.y = 0
+			direction.y = 0
+			direction.x = sign(direction.x)
+			gravity_effect = Vector2.ZERO
+	
+	if !on_floor:
+		quarter_pipe_direction = 0
+	else:
+		gravity_effect = Vector2.ZERO
+	
+	velocity = direction * speed + gravity_effect
+	speed = speed + acceleration
+	
+	if speed > MAX_SPEED:
+		speed = MAX_SPEED
+	
+	position = position + velocity * delta
