bf1580876e22f9f706cbf4023ac156dd0f2d4fa1
Jed McInnes <36739680+McInnesJ@users.noreply.github.com>
2024-10-06 23:54:03 +0100

    Collectables! With sparkle effects when collected

---
+++ Scenes/Collectables/Casset.gd
@@ -0,0 +1,27 @@
+extends Item
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	item_type = ITEM_TYPE.CASSET
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is not Player:
+		return
+	on_item_collected()
+
+func on_item_collected() -> void:
+	
+	$Visuals/Sparkles.emitting = true
+	
+	$Area2D/CollisionShape2D.disabled = true
+	var t = get_tree().create_tween()
+	t.tween_property($Visuals/Sprite2D, "modulate", Color.TRANSPARENT, 0.2)
+	t.tween_interval(0.8)
+	t.tween_callback(queue_free)
+	
+	var menu : Menu = get_tree().root.get_node("Menu")
+	menu.add_item()
