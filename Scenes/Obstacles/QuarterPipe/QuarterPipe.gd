c616aa3735919c26471cdf736439fa38ad802f64
Ciaran Woodward <ciaranwoodward@gmail.com>
2024-10-07 20:50:29 +0100

    Add scenery graphics and make configurable

--- Scenes/Obstacles/QuarterPipe/QuarterPipe.gd
+++ Scenes/Obstacles/QuarterPipe/QuarterPipe.gd
@@ -1,11 +1,53 @@
+@tool
 class_name QuarterPipe
 extends StaticBody2D
+
+const NUM_POINTS = 10
+
+@export var radius = 190 : set = _set_radius
 
 @onready var centre_node = $Centre
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	gen_arc_points($Centre.position, PI/2, PI)
+
+func _clear_collision_shapes():
+	for child in get_children():
+		if child is CollisionShape2D:
+			child.queue_free()
+
+func gen_arc_points(center: Vector2, start_angle: float, end_angle: float):
+	var visual_radius = radius + $Visuals/Line2D.width/2
+	var points = PackedVector2Array() 
+	var angle_step = (end_angle - start_angle) / (NUM_POINTS - 1)
+
+	var prev_collis : Vector2 = Vector2.ZERO
+	for i in range(NUM_POINTS):
+		var angle = start_angle + i * angle_step
+		var x = center.x + visual_radius * cos(angle)
+		var y = center.y + visual_radius * sin(angle)
+		var x_collis = center.x + radius * cos(angle)
+		var y_collis = center.y + radius * sin(angle)
+		points.append(Vector2(x, y))
+		var new_collis = Vector2(x_collis, y_collis)
+		if !prev_collis.is_zero_approx():
+			var new_collis_shape = CollisionShape2D.new()
+			var segshape = SegmentShape2D.new()
+			segshape.a = prev_collis
+			segshape.b = new_collis
+			new_collis_shape.shape = segshape
+			add_child(new_collis_shape)
+		prev_collis = new_collis
+	$Visuals/Line2D.points = points
+	var new_maxbounds = RectangleShape2D.new()
+	new_maxbounds.size = Vector2(radius*1.1, radius*1.1)
+	$MaxBounds/CollisionShape2D.shape = new_maxbounds
+	$MaxBounds/CollisionShape2D.position = Vector2(-radius/2, radius/2) + $Centre.position
+
+func _set_radius(new_rad):
+	radius = new_rad
+	gen_arc_points($Centre.position, PI/2, PI)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -45,4 +87,4 @@ func get_speed_component_at_entrance(global_pos: Vector2, global_dir: Vector2) -
 	return abs(dot / p2.length())
 
 func get_radius() -> float:
-	return get_pipe_centre().distance_to($Exit2.global_position)
+	return radius
