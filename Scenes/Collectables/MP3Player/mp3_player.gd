b8b626d96e7c44c6d75b7e22032b72f21adffaa9
Jed McInnes <36739680+McInnesJ@users.noreply.github.com>
2024-10-07 16:30:09 +0100

    Casset and MP3 collectable types Allow option to set audio file tied to collectable

--- Scenes/Collectables/MP3Player/mp3_player.gd
+++ Scenes/Collectables/MP3Player/mp3_player.gd
@@ -1,8 +1,10 @@
 extends Item
 
+@export var audio_file : AudioStreamOggVorbis
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	item_type = ITEM_TYPE.CASSET
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -12,16 +14,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is not Player:
 		return
 	on_item_collected()
-
+	
 func on_item_collected() -> void:
-	
-	$Visuals/Sparkles.emitting = true
-	
-	$Area2D/CollisionShape2D.disabled = true
-	var t = get_tree().create_tween()
-	t.tween_property($Visuals/Sprite2D, "modulate", Color.TRANSPARENT, 0.2)
-	t.tween_interval(0.8)
-	t.tween_callback(queue_free)
+	on_item_collected_visual_queue()
 	
 	var menu : Menu = get_tree().root.get_node("Menu")
 	menu.add_item()
